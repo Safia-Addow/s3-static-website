ffb52fc79e652b06d0329d85bc6f7513b7482b70
Safia-Addow <122935768+Safia-Addow@users.noreply.github.com>
2026-04-15 10:28:40 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BUCKET_NAME="safiaddow-bucket2026"
+REGION="eu-west-2"
+
+echo "Creating bucket..."
+aws s3 mb s3://$BUCKET_NAME --region $REGION
+
+echo "Enabling static website hosting..."
+aws s3 website s3://$BUCKET_NAME \
+--index-document index.html \
+--error-document error.html
+
+echo "Uploading files..."
+aws s3 sync . s3://$BUCKET_NAME --exclude "README.md" --exclude ".git/*"
+
+echo "Applying bucket policy..."
+aws s3api put-bucket-policy \
+--bucket $BUCKET_NAME \
+--policy file://bucket-policy.json
+
+echo "Disabling block public access..."
+aws s3api put-public-access-block \
+--bucket $BUCKET_NAME \
+--public-access-block-configuration \
+BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false
+
+echo "Deployment complete!"
+echo "Website URL:"
+echo "http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
